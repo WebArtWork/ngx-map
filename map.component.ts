11e71ff6def70326d3f9d9fb890af601b6eb25f0
Vlad <58708136+rooben4ik@users.noreply.github.com>
2022-05-31 12:49:33 +0300

    Add files via upload

--- map.component.ts
+++ map.component.ts
@@ -6,16 +6,32 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class MapComponent{
 	@Output() map_clicked : EventEmitter<any> = new EventEmitter();
-		// output selected pin coords
-		// output map clicked with coords
 	@Input() markers:any=[];
 	@Input() select = false;
 	@Input() lat: number;
 	@Input() lng: number;
+	zoom: number = 13;
+	minZoom: number = 2;
+	public default_lat: number = 51.673858;
+	public default_lng: number = 7.815982;
 	clickMap(coords){
 		this.lat = coords.lat;
 		this.lng = coords.lng;
 		this.map_clicked.emit(coords);
 	}
-	constructor() {}
+	countryRestriction = {
+		latLngBounds: {
+			north: 85,
+			south: -85,
+			west: -179.99,
+			east: 179.99,
+		},
+		strictBounds: true
+	};
+	constructor() {
+		navigator.geolocation.getCurrentPosition((success)=>{
+			this.default_lat = success.coords.latitude;
+			this.default_lng = success.coords.longitude;
+		});
+	}
 }
--- map.module.ts
+++ map.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CoreModule } from '@core';
 import { AgmCoreModule } from '@agm/core';
 import { environment } from 'src/environments/environment';
 import { MapComponent } from './map.component';
-
+import { WacomModule } from 'wacom';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @NgModule({
 	imports: [
-		CoreModule,
+		CommonModule,
+		FormsModule,
+		WacomModule,
 		AgmCoreModule.forRoot({
 			apiKey: environment.google_maps
 		  }),
